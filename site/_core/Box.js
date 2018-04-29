f2f6edaf39efe9490a55b4501bad82303ddd254a
Nassi Harel <nassi9@gmail.com>
2018-04-29 11:14:06 +0300

    updates

--- site/_core/Box.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-module.exports = ({ url, title, text }) =>
-  <div style={{ width: 250 }}>
-    <a className="read-next" href={url}>
-      <span className="read-next-continue">{title} &rarr;</span>
-      <span className="read-next-title">{text}</span>
-    </a>
-  </div>
--- site/algorithms/Algorithms-Thinking.md
+++
@@ -1,9 +0,0 @@
----
-title: Thinking
-layout: ../_core/DocsLayout
-category: Algorithms
-permalink: /algorithms/thinking/
-next: /algorithms/socket/
----
-
-On this page, you'll learn in detail about how 
