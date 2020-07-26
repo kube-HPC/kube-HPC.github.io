16311e5762ba7012538a48c0e054b249e1a86377
reggev <reggev.am@gmail.com>
2020-07-26 16:31:10 +0300

    converted px to ems and setup body font size

--- site/tutorial/index.html.js
+++ site/tutorial/index.html.js
@@ -1,22 +1,35 @@
+var React = require("react");
+var Site = require("../_core/Site");
+var Box = require("../_core/Box");
 
-var React = require('react');
-var Site = require('../_core/Site');
-var Box = require('../_core/Box');
-
-export default ({ page, site }) =>
+export default ({ page, site }) => (
   <Site section="code" title="Code" page={page}>
     <section className="point1" id="predictable-results">
       <div className="documentationContent">
         <div className="inner-content">
-          <h1>Interactive Tutorial</h1>
           <div>
-            <h2 style={{ textAlign: "center" }}>What kind of use?</h2>
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: '50%', margin: '0 auto' }}>
+            <h1 style={{ textAlign: "center" }}>Interactive Tutorial</h1>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "50%",
+                margin: "0 auto",
+              }}
+            >
               <div>
-                <Box url="/tutorial/stand-alone.html" title="&larr; Go To" text="Stand Alone"></Box>
+                <Box
+                  url="/tutorial/stand-alone.html"
+                  title="&larr; Go To"
+                  text="Stand Alone"
+                ></Box>
               </div>
               <div>
-                <Box url="/tutorial/cluster.html" title="Go To &rarr;" text="Kubernetes Cluster"></Box>
+                <Box
+                  url="/tutorial/cluster.html"
+                  title="Go To &rarr;"
+                  text="Kubernetes Cluster"
+                ></Box>
               </div>
             </div>
           </div>
@@ -24,3 +37,4 @@ export default ({ page, site }) =>
       </div>
     </section>
   </Site>
+);
