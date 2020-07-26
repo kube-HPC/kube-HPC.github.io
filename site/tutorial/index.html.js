be6b489a0b797e222d9b4bb32d86ef3cc4c5a50b
reggev <reggev.am@gmail.com>
2020-07-26 16:55:07 +0300

    narrowed layout of tutorial and get started page

--- site/tutorial/index.html.js
+++ site/tutorial/index.html.js
@@ -5,20 +5,23 @@ var Box = require("../_core/Box");
 export default ({ page, site }) => (
   <Site section="code" title="Code" page={page}>
     <section className="point1" id="predictable-results">
-      <div className="documentationContent">
+      <div
+        className="documentationContent"
+        style={{ justifyContent: "center" }}
+      >
         <div className="inner-content">
           <div>
             <h1 style={{ textAlign: "center" }}>Interactive Tutorial</h1>
             <div
               style={{
                 display: "flex",
-                justifyContent: "space-between",
-                width: "50%",
+                justifyContent: "center",
                 margin: "0 auto",
               }}
             >
               <div>
                 <Box
+                  className="tutorial-box"
                   url="/tutorial/stand-alone.html"
                   title="&larr; Go To"
                   text="Stand Alone"
@@ -26,6 +29,7 @@ export default ({ page, site }) => (
               </div>
               <div>
                 <Box
+                  className="tutorial-box"
                   url="/tutorial/cluster.html"
                   title="Go To &rarr;"
                   text="Kubernetes Cluster"
