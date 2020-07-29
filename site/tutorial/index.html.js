b3871a7ba4a8df80a8330a550306d1828900e270
reggev <reggev.am@gmail.com>
2020-07-29 17:54:56 +0300

    fixed tutorial page responsivity issues

--- site/tutorial/index.html.js
+++ site/tutorial/index.html.js
@@ -3,41 +3,22 @@ var Site = require("../_core/Site");
 var Box = require("../_core/Box");
 
 export default ({ page, site }) => (
-  <Site section="code" title="Code" page={page}>
+  <Site section="code" title="Code" page={page} className="tutorial-body">
     <section className="point1" id="predictable-results">
-      <div
-        className="documentationContent"
-        style={{ justifyContent: "center" }}
-      >
-        <div className="inner-content">
-          <div>
-            <h1 style={{ textAlign: "center" }}>Interactive Tutorial</h1>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                margin: "0 auto",
-              }}
-            >
-              <div>
-                <Box
-                  className="tutorial-box"
-                  url="/tutorial/stand-alone.html"
-                  title="&larr; Go To"
-                  text="Stand Alone"
-                ></Box>
-              </div>
-              <div>
-                <Box
-                  className="tutorial-box"
-                  url="/tutorial/cluster.html"
-                  title="Go To &rarr;"
-                  text="Kubernetes Cluster"
-                ></Box>
-              </div>
-            </div>
-          </div>
-        </div>
+      <h1 style={{ textAlign: "center" }}>Interactive Tutorial</h1>
+      <div className="tutorial-buttons">
+        <Box
+          className="tutorial-box"
+          url="/tutorial/stand-alone.html"
+          title="&larr; Go To"
+          text="Stand Alone"
+        />
+        <Box
+          className="tutorial-box"
+          url="/tutorial/cluster.html"
+          title="Go To &rarr;"
+          text="Kubernetes Cluster"
+        />
       </div>
     </section>
   </Site>
