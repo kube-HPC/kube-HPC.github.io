53cc00c348fcedd58d11ba7f5bcdcf099fa11bdf
reggev <reggev.am@gmail.com>
2020-08-19 14:21:46 +0300

    resized home font-size on mobile

--- site/Home/HPC.lead.js
+++ site/Home/HPC.lead.js
@@ -4,7 +4,7 @@ const HPC = () => (
   <section className="lead">
     <div className="lead-body">
       <h1 className="home-lead-header">HPC over Kubernetes</h1>
-      <p className="home-section-content">
+      <p className="home-section-content home-lead-content">
         Hkube is a cloud-native open-source framework to run a distributed
         pipeline of algorithms built on Kubernetes. Hkube allows running
         pipelines of algorithms on Kubernetes cluster optimally utilizing the
